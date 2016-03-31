79b9eddea43ce53e65b7af3c82d6cf1128024248
Daniel Wright <djw67@pitt.edu>
2016-03-31 13:32:17 -0400

    add stuff for labs

---
+++ lab_valgrind/leak.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef
+   struct _Node {
+      struct _Node *l;
+      struct _Node *r;
+   } Node;
+
+Node* mk(void)
+{
+   Node *x = malloc(sizeof(Node));
+   x->l = NULL;
+   x->r = NULL;
+   return x;
+}
+
+Node* t;
+
+void f(void)
+{
+   t       = mk();   // A
+   t->l    = mk();   // B
+   t->r    = mk();   // C
+   t->l->l = mk();   // D
+   t->l->r = mk();   // E
+   t->r->l = mk();   // F
+   t->r->r = mk();   // G
+
+   t->l->l = NULL;
+   t->r = NULL;
+}
+
+int main(void)
+{
+   f();
+   return 0;
+}
